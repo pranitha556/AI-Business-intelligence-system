729f75324b558ea60206846052f11f120a8ff794
pranitha556 <pranithaj556@gmail.com>
2026-05-09 22:52:01 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException 
+from fastapi import File , UploadFile
 from pydantic import BaseModel
 
 from passlib.context import CryptContext
